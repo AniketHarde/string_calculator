05619600ff12566631505a4d32bf67e48731ac93
Aniket Harde <hardeaniket8@gmail.com>
2025-06-12 10:55:21 +0530

    feat: always normalize newlines to commas in StringCalculator#add
    Updated sum_of_numbers to replace newlines with commas before splitting.
    Now add method supports both commas and newlines as delimiters for summing numbers.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,7 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    return sum_of_numbers(numbers) if numbers.include?(",")
-    numbers.to_i
+    sum_of_numbers(numbers)
   end
 
   private
