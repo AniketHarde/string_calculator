76df48602d150b120c4c7be7f19d7c8739645b78
Aniket Harde <hardeaniket8@gmail.com>
2025-06-12 09:28:15 +0530

    feat: update StringCalculator#add to handle single number input
    Modified the add method to return the integer value of the input string.
    Now returns correct results for single number inputs such as "0", "1", and "11".

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe StringCalculator do
     it "is expected to return 0 for '0'" do
       expect(calculator.add("0")).to eq(0)
     end
+
+    it "is expected to return 1 for '1'" do
+      expect(calculator.add("1")).to eq(1)
+    end
+
+    it "is expected to return 11 for '11'" do
+      expect(calculator.add("11")).to eq(11)
+    end
   end
 end
