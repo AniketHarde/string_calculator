afd4e251b3ce10b2f9de065f0a244562ee9f9dfc
Aniket Harde <hardeaniket8@gmail.com>
2025-06-12 08:53:58 +0530

    test: add spec for StringCalculator#add with empty string
    Added a test to expect StringCalculator#add("") to return 0.
    Currently fails with NoMethodError since the add method is not yet implemented:
    
    NoMethodError:
      undefined method `add' for an instance of StringCalculator
    
    This documents the next step for implementation.

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -1,5 +1,9 @@
 require 'string_calculator'
 
 RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
 
+  it "is expected to returns 0 for an empty string" do
+    expect(calculator.add("")).to eq(0)
+  end
 end
